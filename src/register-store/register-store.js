7f7b3df93b6c2a8a6a29344c8f19d0936979eab7
Jamie Nuttall <jamie@wattu.com>
2017-10-02 16:59:56 +0100

    store now pushes values to DB

--- src/register-store/register-store.js
+++ src/register-store/register-store.js
@@ -1,10 +1,28 @@
+import firebase from 'firebase'
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyDo4HOpjUvts6hLHOjDD4ehSkJzUXykNyE', 
+    authDomain: 'phev-db3fa.firebaseapp.com',        
+    databaseURL: 'https://phev-db3fa.firebaseio.com',
+    projectId: 'phev-db3fa',
+    storageBucket: 'phev-db3fa.appspot.com',
+    messagingSenderId: '557258334399'
+  });
+
 const RegisterStore = register => {
     const store = new Array(255)
 
     return {
-        set: register => store[register.register] = { register: register.register, data: register.data },
-        get: id => Promise.resolve(store[id]),
-        display: () => store.forEach(reg => reg != null ? console.log(JSON.stringify(reg)) : '')
+        set: register => {
+            const reg = firebase.database().ref('registers').child(register.register)
+            
+            store[register.register] = { data: register.data }
+            
+            reg.set({ data: register.data, updated: new Date().toJSON() })
+        },   
+        get: id => {
+            return Promise.resolve(store[id])    
+        },
     }
 }
 

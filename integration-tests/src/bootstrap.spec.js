a9308bea5e7b828cfa642016165acad9f76faed8
Jamie Nuttall <jamie@wattu.com>
2017-10-20 15:54:32 +0100

    added int test

--- integration-tests/src/bootstrap.spec.js
+++ integration-tests/src/bootstrap.spec.js
@@ -12,18 +12,26 @@ server.start = sinon.stub()
 server.stop = sinon.stub()
 server.post = sinon.stub()
 
-describe('Integration Tests', ()=> {
+describe('Integration Tests', () => {
     it('Should bootstrap App', () => {
-        const sut = App({ messaging: MessagingClient(), store: LocalStore(), httpApiServer: server})
+        const sut = App({ messaging: MessagingClient(), store: LocalStore(), httpApiServer: server })
         assert.isNotNull(sut)
     })
     it('Should send start message', done => {
         const messaging = MessagingClient()
-        
+
         messaging.eventEmitter.once('outgoing', message => {
-            assert.deepEqual(message, Buffer.from([0xf2,0x0a,0x00,0x01,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0xff]))
+            assert.deepEqual(message, Buffer.from([0xf2, 0x0a, 0x00, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff]))
             done()
         })
-        App({ messaging, store: LocalStore(),httpApiServer: server})
+        App({ messaging, store: LocalStore(), httpApiServer: server })
+    })
+    it('Should send AA message', done => {
+        const messaging = MessagingClient()
+
+        messaging.eventEmitter.on('outgoing', message => {
+            if(message.equals(Buffer.from([0xf6, 0x04, 0x00, 0xaa, 0x00, 0xa4]))) done()
+        })
+        App({ messaging, store: LocalStore(), httpApiServer: server })
     })
 })
--- integration-tests/src/local-pubsub.js
+++ integration-tests/src/local-pubsub.js
@@ -1,11 +1,15 @@
 import EventEmitter from 'events'
 
-const MessagingClient = ({ee = new EventEmitter(), handlers = []} = {}) => ({
+const MessagingClient = ({ee = new EventEmitter(), handlers = [], events =[]} = {}) => ({
     start: () => Promise.resolve(),
     stop: () => Promise.resolve(),
     registerHandler: handler => ee.addListener('incoming',handler),
-    publish: message => ee.emit('outgoing',message),
-    eventEmitter: ee
+    publish: message => {
+        events.push(message)
+        ee.emit('outgoing',message)
+    },
+    eventEmitter: ee,
+    events: events,
 })
 
 export default MessagingClient
